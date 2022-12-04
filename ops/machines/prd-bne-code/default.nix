bc99f480981790b3df805a37fe4e179b02c62b4f
code.dmz.corp <code.dmz.corp@noreply.fediversehosting.com>
2022-12-04 08:32:02 +0000

    abc

--- ops/machines/prd-bne-code/default.nix
+++ ops/machines/prd-bne-code/default.nix
@@ -14,7 +14,6 @@ in
 {
   imports = [
     (mod "defaults.nix")
-    # (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
