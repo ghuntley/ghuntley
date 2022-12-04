b69d5cf68652aabd9296b23c23d3e3e37058ecbd
code.dmz.corp <code.dmz.corp@noreply.fediversehosting.com>
2022-12-04 15:44:01 +0000

    a

--- ops/secrets/secrets.nix
+++ ops/secrets/secrets.nix
@@ -17,6 +17,7 @@ let
   initrd-rescue = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH9wN51lVteNd8hUtx9QerSBZUFVI6K9U1yc3N97wbiv root@rescue";
   prd-bne-ca = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICS26s/wN6eyyM2/JcNAGA74bI54s5ALk0u8mOVbxSz+ root@ca";
   prd-bne-cache = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIObx9zfI6Zk40Dxk2GvBoQzZukj41O5wdf4XnaF5cjOG root@cache";
+  prd-bne-code = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtAec+y9nUZ0bbJmypLf6DKBwEtN7s76rIfwoGhnU8P root@code";
   prd-bne-proxy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEG0iV1HBOqhR8J1AEMWFHB5WeBkMX6HN7n7EPF0Ap1e root@proxy";
   prd-bne-smtp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIQEX+nJtbsJikHs5MqRGfPA1HzbSdqD0LPq9TcmBZdV root@smtp";
   prd-bne-time = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMeKTNstopArcJk5z7Ag6WO8MXlROSxWQ+4cP9rhlpUp root@time";
@@ -27,6 +28,7 @@ let
     initrd-rescue
     prd-bne-ca
     prd-bne-cache
+    prd-bne-code
     prd-bne-proxy
     prd-bne-smtp
     prd-bne-time
@@ -49,10 +51,10 @@ let
 
 in
 {
-  "nix-cache-priv.age" = service-nixcache;
-  "nix-cache-pub.age" = service-nixcache;
-  "smtprelay.age" = service-smtp;
+  # "nix-cache-priv.age" = service-nixcache;
+  # "nix-cache-pub.age" = service-nixcache;
+  # "smtprelay.age" = service-smtp;
   "ssh-initrd-ed25519-key.age" = prdDefault;
   "ssh-initrd-ed25519-pub.age" = prdDefault;
-  "tailscale-ephemeral-server-bootstrap-key.age" = prdDefault;
+  # "tailscale-ephemeral-server-bootstrap-key.age" = prdDefault;
 }
