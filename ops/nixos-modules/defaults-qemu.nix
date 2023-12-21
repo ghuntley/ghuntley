501bfb594831b2c51bda04b85577b260a364107e
ghuntley-dev.servers <ghuntley-dev.servers@noreply.ghuntley.net>
2023-12-21 10:57:40 +0000

    feat(qemu): enable serial console

--- ops/nixos-modules/defaults-qemu.nix
+++ ops/nixos-modules/defaults-qemu.nix
@@ -27,6 +27,10 @@ in
     (mod "netdata.nix")
   ];
 
+  boot.kernelParams = [
+    "console=ttyS0,115200"
+    "console=tty1"
+  ];
   boot.initrd.availableKernelModules = [ "virtio_net" "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_scsi" "9p" "9pnet_virtio" ];
   boot.initrd.kernelModules = [ "virtio_balloon" "virtio_console" "virtio_rng" ];
 
