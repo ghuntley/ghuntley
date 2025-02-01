6d2dbe4139ce8eefb147e8dd4e92de83c3c639d8
Geoffrey Huntley <ghuntley@ghuntley.com>
2025-02-01 08:13:31 +0000

    add cursor

--- users/ghuntley/home/modules/cursor.nix
+++ users/ghuntley/home/modules/cursor.nix
@@ -1,2 +1,6 @@
 # Copyright (c) 2025 Geoffrey Huntley <ghuntley@ghuntley.com>. All rights reserved.
 # SPDX-License-Identifier: Proprietary
+
+{ config, lib, pkgs, ... }:
+
+{ }
--- users/ghuntley/home/modules/shell-alias-cursor.nix
+++ users/ghuntley/home/modules/shell-alias-cursor.nix
@@ -20,4 +20,52 @@
     '';
     executable = true;
   };
+
+  home.file = {
+    ".icons/cursor.png".source = ../icons/cursor.png;
+  };
+
+  xdg.desktopEntries = {
+    "cursor" = {
+      name = "Cursor";
+      genericName = "Text Editor";
+      comment = "AI-first code editor";
+      exec = "${config.home.homeDirectory}/bin/cursor %F";
+      icon = "${config.home.homeDirectory}/.icons/cursor.png";
+      terminal = false;
+      type = "Application";
+      mimeType = [
+        "text/plain"
+        "application/x-text-html"
+        "text/x-c"
+        "text/x-c++"
+        "text/x-c-header"
+        "text/x-c++-header"
+        "text/x-java"
+        "text/x-python"
+        "text/x-javascript"
+        "text/x-typescript"
+        "text/x-php"
+        "text/x-ruby"
+        "text/x-rust"
+        "text/x-go"
+        "text/x-scala"
+        "text/x-swift"
+        "text/x-perl"
+        "text/x-shellscript"
+        "text/x-makefile"
+        "text/x-cmake"
+        "text/x-yaml"
+        "text/x-json"
+        "text/x-sql"
+        "text/xml"
+        "application/xml"
+        "application/json"
+        "application/javascript"
+        "application/x-httpd-php"
+        "application/x-yaml"
+      ];
+      startupNotify = true;
+    };
+  };
 }
