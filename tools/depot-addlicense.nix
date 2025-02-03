b4726e290696fc9ca683b08399e8c409c29c46d4
Geoffrey Huntley <ghuntley@ghuntley.com>
2025-02-03 13:39:45 +0000

    add license vendored

--- tools/depot-addlicense.nix
+++ tools/depot-addlicense.nix
@@ -18,15 +18,15 @@ let
   depot-addlicense = pkgs.writeShellScriptBin "depot-addlicense" ''
     if [ $# -eq 0 ]; then
       # No arguments provided, use DEPOT_ROOT
-      exec ${pkgs.addlicense}/bin/addlicense -f ${config} ''${DEPOT_ROOT}
+      exec ${depot.third_party.addlicense}/bin/addlicense -f ${config} ''${DEPOT_ROOT}
     else
       # Arguments provided, use them as file paths
-      exec ${pkgs.addlicense}/bin/addlicense -f ${config} "$@"
+      exec ${depot.third_party.addlicense}/bin/addlicense -f ${config} "$@"
     fi
   '';
 
   check = pkgs.writeShellScriptBin "depot-addlicense-check" ''
-    exec ${pkgs.addlicense}/bin/addlicense -check -f ${config} ''${DEPOT_ROOT}
+    exec ${depot.third_party.addlicense}/bin/addlicense -check -f ${config} ''${DEPOT_ROOT}
   '';
 
 in
