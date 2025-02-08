827ec9b5cdb149a532fc2faffc29e88e455060f4
Geoffrey Huntley <ghuntley@ghuntley.com>
2025-02-08 08:16:24 +0000

    feat(cursor): add DNS records and conventional commits rules, update DNS config

--- infra/dns/dns-com-ponderoos.tf
+++ infra/dns/dns-com-ponderoos.tf
@@ -251,3 +251,11 @@ resource "cloudflare_record" "com_ponderoos_healthchecks" {
   value   = var.com_ponderoos_ipv4
   proxied = false
 }
+
+resource "cloudflare_record" "com_ponderoos_chat" {
+  zone_id = var.com_ponderoos_cloudflare_zone_id
+  name    = "chat"
+  type    = "A"
+  value   = var.com_ponderoos_ipv4
+  proxied = false
+}
