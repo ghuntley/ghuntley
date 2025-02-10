818895d1ad09664e95fa97974ab247d1952691c2
Geoffrey Huntley <ghuntley@ghuntley.com>
2025-02-10 10:19:32 +0000

    refactor(tools): consolidate depot-fmt into single file

---
+++ tools/depot-fmt.nix
@@ -0,0 +1,75 @@
+# Copyright (c) 2019 Vincent Ambo
+# Copyright (c) 2020-2021 The TVL Authors
+# Copyright (c) 2022 Geoffrey Huntley <ghuntley@ghuntley.com>. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+# Builds treefmt for depot, with a hardcoded configuration that
+# includes the right paths to formatters.
+{ depot
+, pkgs
+, ...
+}:
+
+let
+  # terraform fmt can't handle multiple paths at once, but treefmt
+  # expects this
+  terraformat = pkgs.writeShellScript "terraformat" ''
+    echo "$@" >/tmp/a
+  '';
+
+  config = pkgs.writeText "depot-treefmt-config" ''
+
+    [formatter.go]
+    command = "${pkgs.go}/bin/gofmt"
+    options = [ "-w" ]
+    includes = ["*.go"]
+
+    [formatter.hs]
+    command = "${pkgs.ormolu}/bin/ormolu"
+    includes = ["*.hs"]
+
+    [formatter.nix]
+    command = "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt"
+    includes = [ "*.nix" ]
+
+    [formatter.py]
+    command = "${pkgs.python3.pkgs.black}/bin/black"
+    includes = [ "*.py" ]
+
+    [formatter.sh]
+    command = "${pkgs.shfmt}/bin/shfmt"
+    includes = [ "*.sh" ]
+
+    [formatter.tf]
+    command = "${terraformat}"
+    includes = [ "*.tf" ]
+
+    [formatter.rust]
+    command = "${pkgs.rustfmt}/bin/rustfmt"
+    options = ["--edition", "2021"]
+    includes = [ "*.rs" ]
+  '';
+
+  # helper tool for formatting the depot interactively
+  depot-fmt = pkgs.writeShellScriptBin "depot-fmt" ''
+    exec ${depot.third_party.treefmt}/bin/treefmt ''${@} \
+      --config-file ${config} \
+      --tree-root $(${pkgs.git}/bin/git rev-parse --show-toplevel)
+  '';
+
+  # wrapper script for running formatting checks in CI
+  check = pkgs.writeShellScript "depot-fmt-check" ''
+    ${depot.third_party.treefmt}/bin/treefmt \
+      --clear-cache \
+      --fail-on-change \
+      --config-file ${config} \
+      --tree-root .
+  '';
+in
+depot-fmt.overrideAttrs (_: {
+  passthru.meta.ci.extraSteps.check = {
+    label = "depot formatting check";
+    command = check;
+    alwaysRun = true;
+  };
+})
