d7dff84525cf84789b5002e523f5bce072e4b5d9
Geoffrey Huntley <ghuntley@ghuntley.com>
2025-02-11 20:21:45 +0000

    feat(infra): add geoipupdate service and secrets

---
+++ infra/nixos-modules/geoipupdate.nix
@@ -0,0 +1,72 @@
+# Copyright (c) 2025 Geoffrey Huntley <ghuntley@ghuntley.com>. All rights reserved.
+# SPDX-License-Identifier: Proprietary
+
+# NixOS module for Open WebUI - a web interface for interacting with large language models
+{ depot, pkgs, config, lib, ... }:
+
+let
+  inherit (builtins) attrValues mapAttrs;
+  inherit (lib)
+    concatStringsSep
+    mkEnableOption
+    mkIf
+    mkOption
+    types;
+
+  cfg = config.services.depot.geoipupdate;
+  description = "MaxMind GeoIP Update";
+
+  # Helper functions for argument preparation
+  prepareArgs = args:
+    concatStringsSep " "
+      (attrValues (mapAttrs (key: value: "-${key} \"${toString value}\"")
+        args));
+in
+{
+  options.services.depot.geoipupdate = {
+    enable = mkEnableOption description;
+
+    stateDir = mkOption {
+      type = types.path;
+      default = "/var/lib/GeoIP";
+      description = "Directory where GeoIPUpdate stores its data";
+    };
+
+    accountId = mkOption {
+      type = types.int;
+      description = "MaxMind Account ID";
+      example = "1234567890";
+    };
+
+    licenseKey = mkOption {
+      type = types.path;
+      description = "MaxMind License Key";
+      example = "config.age.secrets.geoipupdate-license-key.path";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.geoipupdate = {
+      enable = true;
+      settings = {
+        AccountID = cfg.accountId;
+        LicenseKey = cfg.licenseKey;
+        EditionIDs = [
+          "GeoLite2-ASN"
+          "GeoLite2-City"
+          "GeoLite2-Country"
+        ];
+        DatabaseDirectory = cfg.stateDir;
+      };
+    };
+
+    # Configure backups using depot.restic
+    services.depot.restic = {
+      enable = true;
+      paths = [
+        cfg.stateDir
+      ];
+      exclude = [ ];
+    };
+  };
+}
